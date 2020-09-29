009556414d52ed9a0e8f961c86b783f3efaa170c
Mohammad ali Ali panah <mohammadali.ap.2000@gmail.com>
2020-09-29 15:47:04 +0330

    Two Sum

--- 1_Two_Number_Sum.py
+++ 1_Two_Number_Sum.py
@@ -1,5 +1,5 @@
 """
-Two Number Sum
+1. Two Sum
 
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -18,4 +18,15 @@ Example 3:
 Input: nums = [3,3], target = 6
 Output: [0,1]
 """
-print('test')
+
+
+# solution 1
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        answers = []
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    answers.append(i)
+                    answers.append(j)
+        return answers
