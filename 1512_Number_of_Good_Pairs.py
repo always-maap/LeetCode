97879bfd32954a5218ad2872ae5547b2c90fa3a3
tur1ng <mohammadali.ap.2000@gmail.com>
2020-09-30 00:16:15 +0330

    Defanging an IP Address

---
+++ 1512_Number_of_Good_Pairs.py
@@ -0,0 +1,30 @@
+"""
+1512. Number of Good Pairs
+Given an array of integers nums.
+A pair (i,j) is called good if nums[i] == nums[j] and i < j.
+Return the number of good pairs.
+
+Example 1:
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+
+Example 2:
+Input: nums = [1,1,1,1]
+Output: 6
+Explanation: Each pair in the array are good.
+
+Example 3:
+Input: nums = [1,2,3]
+Output: 0
+"""
+
+
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        count = 0
+        for i, val in enumerate(nums):
+            for val2 in range(i+1, len(nums)):
+                if val == nums[val2]:
+                    count += 1
+        return count
