0dccd823b6f3e2b38a2bb47f016799c379133f6d
tur1ng <mohammadali.ap.2000@gmail.com>
2020-10-01 16:54:42 +0330

    Jewels and Stones

--- 1108_Defanging_an_IP_Address.py
+++ 1108_Defanging_an_IP_Address.py
@@ -1,4 +1,5 @@
 """
+1431. Kids With the Greatest Number of Candies
 Given a valid (IPv4) IP address, return a defanged version of that IP address.
 A defanged IP address replaces every period "." with "[.]".
 
