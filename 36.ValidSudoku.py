5cbb10c50ffeff3a68addc1f966be3e23867fad3
tur1ng <mohammadali.ap.2000@gmail.com>
2022-07-22 13:35:17 +0430

    valid sudoku

---
+++ 36.ValidSudoku.py
@@ -0,0 +1,76 @@
+"""
+36. Valid Sudoku
+Medium
+Array | Hash Table | Matrix
+---
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+
+Example 1:
+Input: board = 
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+
+Example 2:
+Input: board = 
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+
+Constraints:
+board.length == 9
+board[i].length == 9
+board[i][j] is a digit 1-9 or '.'.
+"""
+
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row_set = defaultdict(set)
+        col_set = defaultdict(set)
+        sec_set = defaultdict(set)
+
+        for i in range(9):
+            for j in range(9):
+                num = board[i][j]
+
+                if num == '.':
+                    continue
+
+                sec = (i // 3, j // 3)
+
+                if num in row_set[i] or num in col_set[j] or num in sec_set[sec]:
+                    return False
+                else:
+                    row_set[i].add(num)
+                    col_set[j].add(num)
+                    sec_set[sec].add(num)
+
+        print(row_set)
+        return True
