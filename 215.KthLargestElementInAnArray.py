11c38d87947d1b86a1038791ef666c9dea4ca02c
tur1ng <mohammadali.ap.2000@gmail.com>
2022-10-13 19:45:47 +0330

    kth largest element in an array

---
+++ 215.KthLargestElementInAnArray.py
@@ -0,0 +1,56 @@
+"""
+215. Kth Largest Element in an Array
+Medium
+Array | Divide and Conquer | Sorting | Heap(Priority Queue) | Quickselect
+---
+Given an integer array nums and an integer k, return the kth largest element in the array.
+
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+You must solve it in O(n) time complexity.
+
+Example 1:
+Input: nums = [3,2,1,5,6,4], k = 2
+Output: 5
+
+Example 2:
+Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+Output: 4
+
+Constraints:
+1 <= k <= nums.length <= 105
+-104 <= nums[i] <= 104
+"""
+
+from typing import List
+
+
+# O(n) time | O(n) space
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        k = len(nums) - k
+
+        def quickSelect(l, r):
+            pivot, p = nums[r], l
+
+            for i in range(l, r):
+                if nums[i] <= pivot:
+                    nums[p], nums[i] = nums[i], nums[p]
+                    p += 1
+            nums[p], nums[r] = nums[r], nums[p]
+
+            if p > k:
+                return quickSelect(l, p - 1)
+            elif p < k:
+                return quickSelect(p + 1, r)
+            else:
+                return nums[p]
+
+        return quickSelect(0, len(nums) - 1)
+
+
+# O(n log n) time | O(1) space
+class Solution2:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        return nums[len(nums)-k]
